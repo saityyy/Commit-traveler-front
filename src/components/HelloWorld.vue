e7ab88fb9398bcee4468e1a21e530e4995a4cb76
saityyy <sec200091@gmail.com>
2021-06-26 18:55:53 +0000

    setting map.svg

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,78 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <svg viewBox="0 0 300 300" width="300" height="300">
+      <line x1="200" y1="100" x2="200" y2="200" stroke="black"></line>
+      <circle cx="100" cy="200" r="50" fill="white" stroke="black"></circle>
+      <rect
+        :x="centerx - scope"
+        :y="centery - scope"
+        :width="2 * scope"
+        :height="2 * scope"
+        fill="#77777777"
+      />
+    </svg>
+    <svg
+      @mousemove="touchmove"
+      @mousedown="canMove = true"
+      @mouseup="canMove = false"
+      :viewBox="viewbox"
+      width="300"
+      height="300"
+    >
+      <line x1="199" y1="100" x2="200" y2="200" stroke="black"></line>
+      <circle cx="100" cy="200" r="50" fill="white" stroke="black"></circle>
+    </svg>
+    <div>
+      <input type="range" v-model="scope" />
+      <p>{{ scope }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  data: function () {
+    return {
+      canMove: false,
+      scope: 150,
+      centerx: 150,
+      centery: 150,
+      prevx: 0,
+      prevy: 0,
+    };
+  },
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  computed: {
+    viewbox: function () {
+      const viewbox = [
+        parseInt(this.centerx) - parseInt(this.scope),
+        parseInt(this.centery) - parseInt(this.scope),
+        2 * this.scope,
+        2 * this.scope,
+      ].join(" ");
+      console.log(viewbox);
+      return viewbox;
+    },
+  },
+  methods: {
+    touchmove(e) {
+      if (this.canMove) {
+        this.centerx = e.offsetX;
+        this.centery = e.offsetY;
+        //this.centerx += this.prevx - e.offsetX;
+        //this.centery += this.prevy - e.offsetY;
+        //console.log(this.centerx);
+        //this.prevx = this.centerx;
+        //this.prevy = this.centery;
+      }
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
 </style>
