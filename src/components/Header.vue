981ee7dc3a194e5519e91784540f13421bbeba3a
Kaito-design <Kaito.m_317@outlook.jp>
2021-07-02 14:19:15 +0000

    time

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,39 @@
 <template>
   <div class="header">
     <div class="mark">codeGo</div>
-    <img src="../assets/icon.png" width="20px" height="20px">
+    <img src="../assets/icon.png" width="20px" height="20px" />
     <div class="name">ようこそ？？？さん</div>
+    <div id="Timer">
+      <p>{{ date }}</p>
+      <p>{{ time }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  el: "#Timer",
+  data: function () {
+    return {
+      date: "",
+      time: "",
+    };
+  },
+
+  mounted: function () {
+    setInterval(this.Time, 1000);
+  },
+
+  methods:{
+    Time: function () {
+      let now = new Date();
+      this.time =
+        now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+      this.date =
+        now.getFullYear() + "/" + (now.getMonth() + 1) + "/" + now.getDate();
+    },
+  },
+};
 </script>
 
 <style>
