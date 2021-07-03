509add26da219903adf6df978e56f386afe52ad5
saityyy <sec200091@gmail.com>
2021-07-03 10:36:35 +0000

    modify CommitInfo

--- src/components/CommitInfo.vue
+++ src/components/CommitInfo.vue
@@ -1,22 +1,48 @@
 <template>
   <div id="commitinfo" v-if="show_flag">
-    <h1>総コミット数</h1>
-    <h2>{{ table_commit }}</h2>
-    <h1>動かせるstep数</h1>
-    <h2>{{ commit }}</h2>
-    <h1>あと{{ user_node.step - user_step }}ステップ</h1>
-    <select v-model="next_node" id="next-node-selector">
-      <option disabled value="">次に移動するマスを選択してください</option>
-      <option v-for="node in user_node.nextNode" :key="node" :value="node">
-        {{ node }}
-      </option>
-    </select>
-    <input v-model="comment" placeholder="ここにコメントを入力してください" />
-    <p>
-      次は<span>{{ next_node }}</span
-      >番のノード
-    </p>
-    <p>コメント :{{ comment }}</p>
+    <h2>
+      動かせるstep数 : <span>{{ commit }}</span>
+    </h2>
+    <h2>
+      次のノードに行くまであと<span>{{ user_node.step - user_step }}</span
+      >ステップ
+    </h2>
+    <div
+      id="select_next_node"
+      v-if="this.commit - (this.user_node.step - this.user_step) >= 0"
+    >
+      <h3>次に進むノードを選択してください</h3>
+      <select v-model="next_node" id="next-node-selector">
+        <option disabled value="">次に移動するマスを選択してください</option>
+        <option v-for="node in user_node.nextNode" :key="node" :value="node">
+          {{ node }}
+        </option>
+      </select>
+    </div>
+    <div id="comment" v-if="next_node_type == 'checkpoint'">
+      <h3>コメントを入力してください</h3>
+      <input v-model="comment" placeholder="ここにコメントを入力してください" />
+    </div>
+    <div id="lang" v-if="next_node_type == 'checkpoint'">
+      <h3>推しの言語を入力してください</h3>
+      <select v-model="programming_language">
+        <option disabled value="">プログラミング言語</option>
+        <option v-for="lang in programming_language_list" :key="lang">
+          {{ lang }}
+        </option>
+      </select>
+    </div>
+    <div id="check" v-if="next_node != 0">
+      <h3>下記の内容でマップを進めます</h3>
+      <p v-if="this.commit - (this.user_node.step - this.user_step) >= 0">
+        ノード{{ this.user_node.id }} => ノード{{ this.next_node }}
+      </p>
+      <p v-else>step{{ this.user_step }} => step{{ this.commit }}</p>
+      <p v-if="next_node_type == 'checkpoint'">コメント :{{ comment }}</p>
+      <p v-if="next_node_type == 'checkpoint'">
+        推しの言語 :{{ programming_language }}
+      </p>
+    </div>
     <input type="button" @click="update_next_node" id="button" />
     <input
       type="button"
@@ -40,7 +66,10 @@ export default {
       user_step: 0,
       show_flag: false,
       next_node: 0,
+      next_node_type: "node",
       comment: "",
+      programming_language_list: [],
+      programming_language: "",
     };
   },
   props: {
@@ -64,6 +93,19 @@ export default {
         .catch((e) => {
           console.log(e);
         });
+      this.axios
+        .get("http://localhost:3000/api/get-langs")
+        .then((res) => {
+          console.log("get-langs");
+          console.log(res.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    next_node(updatedInfo) {
+      if (updatedInfo > 0)
+        this.next_node_type = mapdata[parseInt(updatedInfo) - 1].type;
     },
   },
   computed: {
@@ -75,6 +117,7 @@ export default {
         else {
           if (mapdata[parseInt(this.next_node) - 1].type == "checkpoint") {
             if (this.comment == "") return true;
+            else if (this.programming_language == "") return true;
             else return false;
           } else {
             return false;
